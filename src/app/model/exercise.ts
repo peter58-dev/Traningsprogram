0036d9d2dc29daeecaf0b19a6c4428db7de2b495
peter-lelkes <p.lelkes@gmail.com>
2025-05-26 17:45:10 +0200

    ExerciseInterface created

---
+++ src/app/model/exercise.ts
@@ -0,0 +1,16 @@
+export interface ExerciseInterface {
+  id: string;
+  exerciseName: string;
+  firstSet?: number;
+  weightFirstSet?: string;
+  repsFirstSet?: string;
+  secondSet?: number;
+  repsSecondSet?: string;
+  weightSecondSet?: string;
+  thirdSet?: number;
+  repsThirdSet?: string;
+  weightThirdSet?: string;
+  platesFirstSet?: string;
+  platesSecondSet?: string;
+  platesThirdSet?: string;
+}
