73cf56eed4e009953ab92c00a3962f31c05530ad
peter-lelkes <p.lelkes@gmail.com>
2025-05-26 18:34:21 +0200

    stilar + theme

--- src/app/services/workout.service.ts
+++ src/app/services/workout.service.ts
@@ -1,9 +1,25 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject, signal } from '@angular/core';
+import { Firestore, collection, getDocs } from '@angular/fire/firestore';
+import { ExerciseInterface } from '../model/exercise';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class WorkoutService {
+  private firestore = inject(Firestore);
+  private workoutCollection = collection(this.firestore, 'exerciseProgram');
 
-  constructor() { }
+  //signal för att lagra övningar
+  workouts = signal<ExerciseInterface[]>([]);
+
+  constructor() {}
+
+  async loadWorkouts() {
+    const querySnapshot = await getDocs(this.workoutCollection);
+    this.workouts.set(
+      querySnapshot.docs.map(
+        (doc) => ({ id: doc.id, ...doc.data() } as ExerciseInterface)
+      )
+    );
+  }
 }
