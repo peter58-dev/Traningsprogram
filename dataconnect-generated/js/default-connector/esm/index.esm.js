1d18f581599b14bea808dba6c6b855b103d94308
peter-lelkes <p.lelkes@gmail.com>
2025-05-26 17:40:09 +0200

    general setup + firebase connection

---
+++ dataconnect-generated/js/default-connector/esm/index.esm.js
@@ -0,0 +1,7 @@
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'workouts',
+  location: 'us-central1'
+};
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,17 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
+import { getDataConnect, provideDataConnect } from '@angular/fire/data-connect';
+import { connectorConfig } from '@firebasegen/default-connector';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, provideFirebaseApp(() => initializeApp({ projectId: "strength-app-cd974", appId: "1:588939477744:web:16a9334867c35fde51f835", storageBucket: "strength-app-cd974.firebasestorage.app", apiKey: "AIzaSyCYCOMRjP5uw05meHp0S3TDPQjaSl2LPKs", authDomain: "strength-app-cd974.firebaseapp.com", messagingSenderId: "588939477744" })), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideDatabase(() => getDatabase()), provideDataConnect(() => getDataConnect(connectorConfig))],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
