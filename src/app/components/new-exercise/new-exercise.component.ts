375442056a8e776ab6f9c327d3d0fa4b2611f1b1
peter-lelkes <p.lelkes@gmail.com>
2025-05-28 12:37:29 +0200

    new-exercise component created

---
+++ src/app/components/new-exercise/new-exercise.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new-exercise',
+  standalone: false,
+  templateUrl: './new-exercise.component.html',
+  styleUrls: ['./new-exercise.component.scss'],
+})
+export class NewExerciseComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
