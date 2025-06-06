2107008065402bcf633ee007117653ad2e62a54c
peter-lelkes <p.lelkes@gmail.com>
2025-06-06 10:44:46 +0200

    created NewSetComponent

---
+++ src/app/components/new-set/new-set.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new-set',
+  templateUrl: './new-set.component.html',
+  styleUrls: ['./new-set.component.scss'],
+})
+export class NewSetComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
