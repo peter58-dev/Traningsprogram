5af1c1346b8ffbc946dc5bf07c0e5a93e9366724
peter-lelkes <p.lelkes@gmail.com>
2025-06-06 11:31:20 +0200

    the form newSetForm created as a signal

--- src/app/components/new-set/new-set.component.ts
+++ src/app/components/new-set/new-set.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, inject, signal } from '@angular/core';
+import { FormBuilder, Validators, FormArray, FormGroup } from '@angular/forms';
+import { ModalController } from '@ionic/angular';
+import { WorkoutService } from '../../services/workout.service';
 
 @Component({
   selector: 'app-new-set',
@@ -7,7 +10,38 @@ import { Component, OnInit } from '@angular/core';
   standalone: false,
 })
 export class NewSetComponent implements OnInit {
+  private modalCtrl = inject(ModalController);
+  private fb = inject(FormBuilder);
+  private workoutService = inject(WorkoutService);
+  @Input() exerciseId: string | undefined;
+
   constructor() {}
 
   ngOnInit() {}
+  newSetForm = signal(
+    this.fb.group({
+      sets: this.fb.array([
+        this.fb.group({
+          setNumber: [1],
+          weight: ['', Validators.required],
+          discs: [''],
+          reps: ['', Validators.required],
+        }),
+      ]),
+    })
+  );
+  cancel() {
+    return this.modalCtrl.dismiss(null, 'cancel');
+  }
+
+  confirm() {
+    if (this.newExerciseForm().valid) {
+      this.modalCtrl.dismiss(
+        {
+          exerciseName: this.newExerciseForm().value.exerciseName,
+        },
+        'confirm'
+      );
+    }
+  }
 }
