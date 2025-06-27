56bbc420cd90b89002a1cda3d06500dcf46a2e23
peter-lelkes <p.lelkes@gmail.com>
2025-06-27 03:09:17 +0200

    refactor: extract firebase config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,16 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
-  production: false
-};
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCcTiR8ink31MsFcuJTofl2bnjqRVW_9-E',
 
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
+    authDomain: 'training-app-7dc19.firebaseapp.com',
+
+    projectId: 'training-app-7dc19',
+
+    storageBucket: 'training-app-7dc19.firebasestorage.app',
+
+    messagingSenderId: '316754181932',
+
+    appId: '1:316754181932:web:0af39fb32b90d32936b689',
+  },
+};
