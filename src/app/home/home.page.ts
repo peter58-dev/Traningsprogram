198d570056bf5a30129b6a4672ad729e9c5e57ca
peter58-dev <used.for.dev@gmail.com>
2025-07-01 01:15:43 +0200

    chore: grundläggande home.page.ts

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,55 +1,11 @@
-import { Component, OnInit, inject } from '@angular/core';
-import { WorkoutService } from '../services/workout.service';
-import { ModalController } from '@ionic/angular';
-import { NewExerciseComponent } from '../components/new-exercise/new-exercise.component';
-import { NewSetComponent } from '../components/new-set/new-set.component';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
   standalone: false,
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
 })
-export class HomePage implements OnInit {
-  workoutService = inject(WorkoutService);
-  private ctrlModal = inject(ModalController);
-
-  id!: string;
-
+export class HomePage {
   constructor() {}
-
-  async ngOnInit() {
-    await this.workoutService.loadWorkouts();
-  }
-
-  async openNewExercise() {
-    const modal = await this.ctrlModal.create({
-      component: NewExerciseComponent,
-    });
-    await modal.present();
-    const { data, role } = await modal.onWillDismiss();
-    if (role === 'confirm' && data?.exerciseName) {
-      // await this.workoutService.addNewExercise(data.exerciseName);
-    }
-  }
-
-  async delete(id: string) {
-    // await this.workoutService.deleteExercise(id);
-  }
-
-  addNewWorkout() {
-    console.log('CLICKAD!');
-  }
-  async openNewSet(exerciseId: string) {
-    const modal = await this.ctrlModal.create({
-      component: NewSetComponent,
-      componentProps: { exerciseId },
-    });
-    await modal.present();
-
-    const { data, role } = await modal.onWillDismiss();
-    if (role === 'confirm' && data?.newSet) {
-      // await this.workoutService.addSet(exerciseId, data.newSet);
-    }
-  }
 }
