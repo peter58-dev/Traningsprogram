32b71fc1a7694063253d27130c2f0575744ae1b6
peter-lelkes <p.lelkes@gmail.com>
2025-07-02 21:41:36 +0200

    chore: uppdaterat declarations med MessageListComponent + MessageFormComponent

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { HomePage } from './home.page';
+import { MessageListComponent } from '../components/message-list/message-list.component';
 
 import { HomePageRoutingModule } from './home-routing.module';
+import { MessageFormComponent } from '../components/message-form/message-form.component';
 
 @NgModule({
   imports: [CommonModule, FormsModule, IonicModule, HomePageRoutingModule, ReactiveFormsModule],
-  declarations: [HomePage],
+  declarations: [HomePage, MessageListComponent, MessageFormComponent],
 })
 export class HomePageModule {}
