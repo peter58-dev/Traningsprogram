ba0e71e05075f51b579c6fd3da3a6b947ce12af3
peter-lelkes <p.lelkes@gmail.com>
2025-07-03 10:47:41 +0200

    chore(home) öppna newMessageModal

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component, signal } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { MessageFormComponent } from '../components/message-form/message-form.component';
 
 @Component({
   selector: 'app-home',
@@ -8,9 +10,16 @@ import { Component, signal } from '@angular/core';
 })
 export class HomePage {
   visaMeddelanden = signal(false);
-  constructor() {}
+  constructor(private modalCtrl: ModalController) {}
 
   toggleMeddelanden() {
     this.visaMeddelanden.update((value: boolean) => !value);
+  }
+
+  async openNewMessModal() {
+    const modal = await this.modalCtrl.create({
+      component: MessageFormComponent,
+    });
+    modal.present();
   }
 }
