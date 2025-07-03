03e2982667d5e3b59629dd35f40644745b7a31da
peter-lelkes <p.lelkes@gmail.com>
2025-07-03 17:36:46 +0200

    chore: skapat " meddelanden = signal<Message[]>([]); "

--- src/app/services/message.service.ts
+++ src/app/services/message.service.ts
@@ -1,9 +1,10 @@
-import { Injectable } from '@angular/core';
+import { Injectable, signal } from '@angular/core';
+import { Message } from '../models/message.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MessageService {
-
-  constructor() { }
+  meddelanden = signal<Message[]>([]);
+  constructor() {}
 }
