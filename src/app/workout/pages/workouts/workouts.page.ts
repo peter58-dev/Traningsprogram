520a3982f889646b0757b47f743d286475babf3f
peter-lelkes <p.lelkes@gmail.com>
2025-07-11 18:17:34 +0200

    Completed the list of training programs

--- src/app/workout/pages/workouts/workouts.page.ts
+++ src/app/workout/pages/workouts/workouts.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ProgramService } from 'src/app/shared/services/program.service';
+import { AddWorkoutComponent } from '../../components/add-workout/add-workout.component';
+import { ModalController, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-workouts',
@@ -7,20 +9,51 @@ import { ProgramService } from 'src/app/shared/services/program.service';
   styleUrls: ['./workouts.page.scss'],
   standalone: false,
 })
-export class WorkoutsPage implements OnInit, OnDestroy {
+export class WorkoutsPage implements OnDestroy {
   programs = this.workoutService.programs;
 
-  constructor(private workoutService: ProgramService) {}
+  constructor(
+    private workoutService: ProgramService,
+    private toastCtrl: ToastController,
+    private modalCtrl: ModalController
+  ) {}
 
-  ngOnInit(): void {
-    this.workoutService.initTrainingProgramListener();
+  ngOnDestroy(): void {
+    this.workoutService.stopTrainingProgramsListener();
+  }
+
+  async showProgramSavedToast() {
+    const toast = await this.toastCtrl.create({
+      message: 'Program tillagt!',
+      duration: 1200,
+      position: 'middle',
+      cssClass: 'green-toast',
+    });
+    await toast.present();
+    return toast.onDidDismiss();
+  }
+
+  async openAddModal() {
+    const modal = await this.modalCtrl.create({
+      component: AddWorkoutComponent,
+      backdropDismiss: true,
+      cssClass: 'modal-home',
+    });
+
+    await modal.present();
+    const { data, role } = await modal.onWillDismiss();
+
+    if (role === 'confirm' && data) {
+      try {
+        await this.workoutService.addProgram(data);
+        await this.showProgramSavedToast();
+      } catch (error) {
+        console.error('Error saving program to Firestore:', error);
+      }
+    }
   }
 
   deleteWorkout(id: string) {
     this.workoutService.deleteProgram(id);
-  }
-
-  ngOnDestroy(): void {
-    this.workoutService.stopTrainingProgramsListener();
   }
 }
