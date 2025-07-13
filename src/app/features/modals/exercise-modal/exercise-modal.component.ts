aff0a0a89b5b155323322240ec6855012785467e
peter-lelkes <p.lelkes@gmail.com>
2025-07-13 17:42:16 +0200

    created exercise-modal

---
+++ src/app/features/modals/exercise-modal/exercise-modal.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-exercise-modal',
+  templateUrl: './exercise-modal.component.html',
+  styleUrls: ['./exercise-modal.component.scss'],
+})
+export class ExerciseModalComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {
+    console.log('fungerar');
+  }
+}
