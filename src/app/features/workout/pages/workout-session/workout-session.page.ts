3c01ac534fc0b8647bd4146f80527f20982f5f76
peter-lelkes <p.lelkes@gmail.com>
2025-07-13 13:27:30 +0200

    standalone: false

---
+++ src/app/features/workout/pages/workout-session/workout-session.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-workout-session',
+  templateUrl: './workout-session.page.html',
+  standalone: false,
+  styleUrls: ['./workout-session.page.scss'],
+})
+export class WorkoutSessionPage implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+}
