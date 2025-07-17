cb36e17aaa81e8eacb92e0d10aabfee63276765b
peter-lelkes <p.lelkes@gmail.com>
2025-07-18 00:52:11 +0200

    ett enkel lista med workouts

--- src/app/features/workout/pages/workout-list/workout-list.page.ts
+++ src/app/features/workout/pages/workout-list/workout-list.page.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Program } from 'src/app/shared/models/program.model';
+import { ProgramService } from 'src/app/shared/services/program.service';
 
 @Component({
   selector: 'app-workout-list',
@@ -6,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./workout-list.page.scss'],
   standalone: false,
 })
-export class WorkoutListPage implements OnInit {
-  constructor() {}
+export class WorkoutListPage implements OnDestroy {
+  workouts = this.programService.programs;
 
-  ngOnInit() {
-    console.log('fungerar');
+  constructor(private programService: ProgramService) {
+    this.programService.initTrainingProgramListener();
+  }
+
+  ngOnDestroy(): void {
+    this.programService.stopTrainingProgramsListener();
+  }
+
+  deleteWorkout(id: string) {
+    this.programService.deleteProgram(id);
   }
 }
